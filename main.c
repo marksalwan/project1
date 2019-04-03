ad516f5481b5bc9b93062cae71b8be5492ea72c7
Mark Salwan <mark.salwan@uon.edu.au>
2019-04-03 00:12:30 +0000

    char data type was added

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
 #include <stdio>
 
 int main () {
+    
+    char numbers[100] ;
+    char counter;
+    for (counter = 0; counter < 100; counter++) {
+    letter[counter] = 0;
+    }
+
+
+
+    printf("Enter encryption message" );
+    
+    
+    scanf("%d ")
+    
+    
 
   return 0;
 }
